5a16ba1a9789a2afd7d0a03cbc241014276ce114
artems2911-hub <artem.s.2911@gmail.com>
2026-01-24 11:25:44 +0300

    Ping_pong.py

--- Ping_pong.py
+++ Ping_pong.py
@@ -7,6 +7,32 @@ window = display.set_mode((win_width, win_height))
 display.set_caption("Пинг понг")
 background = transform.scale(image.load("background_pic.jpg"), (win_width, win_height))
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, image_x, image_y, player_x, player_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (image_x, image_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_l(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_w] and self.rect.y >= 0:
+            self.rect.y -= self.speed
+        if keys_pressef[K_s] and self.rect.y <= 440:
+            self.rect.y += self.speed
+    
+    def update_r(self):
+        if keys_pressed[K_UP] and self.rect.y >= 0:
+            self.rect.y -= self.speed
+        if keys_pressed[K_DOWN] and self.rect.y <= 440:
+            self.rect.y += self.speed
+
 clock = time.Clock()
 FPS = 60
 game = True
